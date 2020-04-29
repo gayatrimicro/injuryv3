563f74eeadf8530e0fbadba223979e8ff1afc2c8
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-04-29 13:09:52 +0530

    Injury and pain, contact-us

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
 <link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <link href="assets/css/animate.min.css" rel="stylesheet" type="text/css">
 <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-<link href="assets/css/plugins.css" rel="stylesheet" type="text/css">
+<!-- <link href="assets/css/plugins.css" rel="stylesheet" type="text/css"> -->
 <link href="assets/css/default.css" rel="stylesheet" type="text/css">
 <link href="assets/css/components.css" id="style_components" rel="stylesheet" type="text/css">
 <link href="assets/css/custom.css" rel="stylesheet" type="text/css">
