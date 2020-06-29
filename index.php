44d502b8690a52578c12cec31a9ce4322d869a15
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-06-29 13:38:52 +0530

    mail domain name

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
 <head>
   
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
+<script>
+  dataLayer = [];
+</script>
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WWHGLNP');</script>
+
 <!-- End Google Tag Manager -->
 
 <meta name="google-site-verification" content="bTYuHmF1wY3IOkwm46lCKSz85K6XmrUzVqMaqJgIP1g" />
@@ -702,7 +705,7 @@ if ($('#req-name').val() != "" && $('#req-mobile').val() != "" && $('#req-messag
 
                               // $("#signup").trigger('reset');
                               alert("Your enquiry has been sent successfully");
-
+                              dataLayer.push({'event': 'successR'}); 
                               $(".close").click();  
                               }
   
